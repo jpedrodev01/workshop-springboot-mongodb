6b044769c13fdb7e9c41f0a7a3b442074d8f23f1
João Pedro <jpedrodev01@gmail.com>
2025-07-14 08:37:19 -0300

    Project created

---
+++ src/test/java/com/joaopedro/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaopedro.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
